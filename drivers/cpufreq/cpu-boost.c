7907ac09bad4997310e4d25eca55cb5f07bb87c0
Saravana Kannan <skannan@codeaurora.org>
2014-02-13 19:08:21 -0800

    cpufreq: cpu-boost: Don't register for cpufreq notifiers too early
    The cpufreq notifiers should be registered only after all the data
    structures used in the notifier callbacks have been initialized. So, move
    the cpufreq notifier registration to a later point in the init function.
    
    Change-Id: I043ab5bc0ebb98164c40549fe151a8d801c8c186
    Signed-off-by: Saravana Kannan <skannan@codeaurora.org>
    Signed-off-by: revjunkie <gadgetjuan@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -324,7 +324,7 @@ static int cpu_boost_init(void)
 	int cpu, ret;
 	struct cpu_sync *s;
 
-	cpufreq_register_notifier(&boost_adjust_nb, CPUFREQ_POLICY_NOTIFIER);
+	
 
 	cpu_boost_wq = alloc_workqueue("cpuboost_wq", WQ_HIGHPRI, 0);
 	if (!cpu_boost_wq)
@@ -342,6 +342,7 @@ static int cpu_boost_init(void)
 		s->thread = kthread_run(boost_mig_sync_thread, (void *)cpu,
 					"boost_sync/%d", cpu);
 	}
+	cpufreq_register_notifier(&boost_adjust_nb, CPUFREQ_POLICY_NOTIFIER);
 	atomic_notifier_chain_register(&migration_notifier_head,
 					&boost_migration_nb);
 
